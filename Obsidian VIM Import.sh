a0b23c3ab270620282aea308aa64169e6c12bd32
Colin Pinney <colinpinney@icloud.com>
2020-10-05 22:42:12 +0700

    fixed missing words

--- Obsidian VIM Import.sh
+++ Obsidian VIM Import.sh
@@ -8,12 +8,12 @@ fi
 rm -rf $ZETTEL_PATH/Backup/*
 cp -R $ZETTEL_PATH/Odin $ZETTEL_PATH/Backup/
 NUM_NOTES=($ZETTEL_PATH/Odin/*.md)
-NUM_NOTES=$(${#NUM_NOTES[@]}-$NUM_FIXED_NOTES)
-ls -tpr $ZETTEL_PATH/Odin/ | grep -v '/$' | tail -n +$($NUM_FIXED_NOTES+1) | tr '\n' '\0' | xargs -0 rm -- 
+NUM_NOTES=$((${#NUM_NOTES[@]}-$NUM_FIXED_NOTES))
+ls -tpr $ZETTEL_PATH/Odin/ | grep -v '/$' | tail -n +$(($NUM_FIXED_NOTES+1)) | tr '\n' '\0' | xargs -0 rm -- 
 cp -R $ZETTEL_PATH/Drop\ Box $ZETTEL_PATH/Backup/
 unzip -qq $ZETTEL_PATH/Drop\ Box/Zettelkasten.goodnotes -d $ZETTEL_PATH/Drop\ Box/GoodNotes/
 {
-	seq $($NUM_NOTES+1) | xargs -I read
+	seq $(($NUM_NOTES+1)) | xargs -I read
 	while IFS="" read -r p || [ -n "$p" ]
 	do
 		p=$(echo $p | cut -c2-38)
